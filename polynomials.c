18ae886b115f0b9b1ae57192234b37b27f14ee04
David Freifeld <freifeld.david@gmail.com>
2020-10-18 16:55:49 -0700

    UDP server for verifier

--- polynomials.c
+++ polynomials.c
@@ -1,7 +1,55 @@
 #include <pthread.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 5000
+#define BUFSIZE 1024
 
 void* start_verifier(void* args)
 {
+    // Intialize socket with AF_INET IP family and SOCK_DGRAM datagram service, exit if failed
+    int s;
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        exit(1);
+    }
+    // Establish sockaddr_in struct to pass into bind function
+    struct sockaddr_in addr;
+    memset((char *)&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET; // Specify address family.
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY just 0.0.0.0, machine IP address
+    addr.sin_port = htons(PORT); // Specify port.
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        exit(1);
+    }
+
+    // IP address of client
+    struct sockaddr_in remaddr;
+    socklen_t addrlen = sizeof(remaddr);
+    int recvlen;
+    unsigned char buf[BUFSIZE];
+
+    int connections = 0;
+    int clients[500];
+    int unproven = 1;
+    while (unproven) {
+        buf[recvlen] = 0; // Need a null-terminator!
+        recvlen = recvfrom(s, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+        if (recvlen > BUFSIZE) sendto(s, "ERR: Too long.", 14, 0, (struct sockaddr *) &remaddr, addrlen);
+        else if (recvlen > 0) {
+            printf(" SERVER │ Received %d-byte message from %i: \"%s\"\n", recvlen, remaddr.sin_port, buf);
+            if (strcmp((const char*) buf, "Begin proof.")==0) {
+                clients[connections] = remaddr.sin_port;
+                connections++;
+            }
+        }
+    }
+    
     return 0x0;
 }
 
